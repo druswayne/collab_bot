c3bb53825326bc51a66ab03e2afa3a44db3e92e2
delarsen <92631995+delarsen@users.noreply.github.com>
2024-11-22 19:43:21 +0300

    Новые списки и регистрация

--- Medecines and lists/pythonProject1/main.py
+++ Medecines and lists/pythonProject1/main.py
@@ -4,13 +4,22 @@ import sqlite3
 con = sqlite3.connect('Задание 1.db', check_same_thread=False)
 cursor = con.cursor()
 app = Flask(__name__)
+cursor.execute('SELECT * from medicines')
+data = cursor.fetchall()
 
-
+@app.route("/regestration/")
+def registr():
+    global YourName
+    YourName = request.args.get("name")
+    age = request.args.get("age")
+    choice_tv = request.args.get("med1")
+    choice_chid = request.args.get("med2")
+    choice_ma = request.args.get("med3")
+    choice_kap = request.args.get("med4")
+    return render_template("registration.html")
 @app.route("/")
 def page_medecines():
-    cursor.execute('SELECT * from medicines')
-    data = cursor.fetchall()
-    return render_template("list.html", data= data)
+    return render_template("list.html", data= data, name= YourName)
 
 @app.route("/find/")
 def search_page():
@@ -18,7 +27,7 @@ def search_page():
 
 @app.route("/result_find/")
 def find_page():
-    name = request.args.get("name")
-    return f"Вы искали {name}"
+    NMed = request.args.get("name")
+    return render_template("result.html", NMed= NMed)
 
 app.run(debug=True)
