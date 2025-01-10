716f3849ae5bdb66593f7ecf2e8575f4f14ae20c
delarsen <92631995+delarsen@users.noreply.github.com>
2025-01-10 19:44:40 +0300

    end

--- Medecines and lists/pythonProject1/main.py
+++ Medecines and lists/pythonProject1/main.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, request
 import json
 import sqlite3
-con = sqlite3.connect('Задание 1.db', check_same_thread=False)
-cursor = con.cursor()
 app = Flask(__name__)
-cursor.execute('SELECT * from medicines')
-data = cursor.fetchall()
 
+@app.route("/")
+def loginn():
+    return render_template("login.html")
 @app.route("/regestration/")
 def registr():
-    global YourName
     YourName = request.args.get("name")
     age = request.args.get("age")
     choice_tv = request.args.get("med1")
@@ -17,17 +15,18 @@ def registr():
     choice_ma = request.args.get("med3")
     choice_kap = request.args.get("med4")
     return render_template("registration.html")
-@app.route("/")
-def page_medecines():
-    return render_template("list.html", data= data, name= YourName)
 
-@app.route("/find/")
-def search_page():
-    return render_template("BaseList.html")
+@app.route("/main/")
+def login():
+    return render_template("main_page.html")
 
-@app.route("/result_find/")
-def find_page():
-    NMed = request.args.get("name")
-    return render_template("result.html", NMed= NMed)
+#@app.route("/find/")
+#def search_page():
+#    return render_template("BaseList.html")
+#
+#@app.route("/result_find/")
+#def find_page():
+#    NMed = request.args.get("name")
+#    return render_template("result.html", NMed= NMed)
 
 app.run(debug=True)
